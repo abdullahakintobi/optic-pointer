56e3c16297bff78d41e5abe59a59171002154a9b
abdullahakintobi <akintobiabdullah@gmail.com>
2025-07-05 20:10:10 +0100

    feat: Print 'Click' if the differences between upper and lower eyelids is less than 0.004

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ while True:
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
 
-        print(left[0].y - left[1].y)
+        if (left[0].y - left[1].y) < 0.004:
+            print("Click")
 
     cv2.imshow("Eye controlled mouse", frame)
     cv2.waitKey(1)
